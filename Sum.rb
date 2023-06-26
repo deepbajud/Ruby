54ef93370256812942db9176ad82201f4853428e
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-06-26 22:13:26 +0530

    sum
    sum of two integer numbers

---
+++ Sum.rb
@@ -0,0 +1,20 @@
+# get number 1 from user and store in num1 variable
+puts "Enter first value: "
+num1=gets.chomp.to_i
+
+# get number 2 from user and store in num1 variable
+puts "Enter second value: "
+num2=gets.chomp.to_i
+
+# formula of sum of two number
+sum=num1+num2
+
+# print sum of two numbers
+puts "The sum = #{sum}"
+
+
+
+output 
+first value = 2 
+second value = 2
+the sum = 4
